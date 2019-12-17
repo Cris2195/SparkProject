5a334d17b3514aab96125cfd3ae225e0b2465d26
cristian <cristianciarrocchi2@gmail.com>
2019-12-17 12:57:12 +0100

    added actor and repo

---
+++ orgsparksecondoanno/src/main/scala/MainCristian.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions._
+object MainCristian {
+  def main(args: Array[String]) {
+
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("SparkProject")
+
+    val context = new SparkContext(sparkConf)
+    val sqlContext = new HiveContext(context)
+    import sqlContext.implicits._
+
+    val dfJson = sqlContext.read.json("C:\\Users\\Studente\\Desktop\\filee.json")
+   // dfJson.show()
+    dfJson.dtypes.foreach(println)
+
+
+  }
+}
+
